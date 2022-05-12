3e9a88235ef0861ce690de8692e850e77f6699ee
DanielSalvadorP <102434423+DanielSalvadorP@users.noreply.github.com>
2022-05-12 00:35:12 -0500

    Initial commit

---
+++ app/src/main/java/com/cursokotlin/motroad/MainActivity.kt
@@ -0,0 +1,54 @@
+package com.cursokotlin.motroad
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import com.cursokotlin.motroad.R.id
+
+
+class MainActivity : AppCompatActivity() {
+
+    var editUsuario: EditText?=null;
+    var editClave: EditText?=null;
+    var botonAutenticar: Button?= null;
+    private var mauth: FirebaseAuth?=null;
+
+   public override fun onCreate(savedInstanceState: Bundle?) {
+
+        setTheme(R.style.AppTheme)
+        Thread.sleep(2000)
+        super.onCreate(savedInstanceState)
+        mauth = FirebaseAuth.getInstance();
+        setContentView(R.layout.activity_main);
+        botonAutenticar=findViewById(id.signIn);
+       var editUsuario:EditText= findViewById(id.editTextTextEmailAddress);
+       var editClave:EditText= findViewById(id.editTextTextPassword);
+       botonAutenticar?.setOnClickListener{
+           mauth!!.signInWithEmailAndPassword(editUsuario.text.toString(),editClave.text.toString()).addOnCompleteListener(
+               this
+           ){
+               task->
+                   if(task.isSuccessful){
+                       val user=mauth!!.currentUser
+                       Toast.makeText(this, "Autenticado", Toast.LENGTH_SHORT).show()
+                       startActivity(Intent(this, Inicio::class.java).putExtra("uid",user?.uid))
+
+                   }else{
+                       Log.e("Autenticacion","Fallo en autenticacion",task.exception)
+                           Toast.makeText(this, "Conexion fallidas", Toast.LENGTH_SHORT).show()
+                   }
+
+           }
+       }
+
+    }
+
+}
